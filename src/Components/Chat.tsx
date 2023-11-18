695083a6728dadfbc2cee6e52810af3584d90767
HashRei <hashrei@protonmail.com>
2023-11-18 13:48:55 +0100

    feat: update design of Create and Chat element

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,12 +1,32 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "@mui/material";
 // src/components/About.js
 const Chat = () => {
-    return (
-      <div>
-        {"Add the chat component here"}
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6 py-8">
+        <div className="bg-white rounded-xl shadow-xl p-8 md:p-12 lg:p-16 w-full max-w-4xl">
+          <div className="text-center mb-8 md:mb-12 space-y-4">
+            <h1 className="text-4xl md:text-5xl font-bold mb-4">
+              Logo for web3 startup
+            </h1>
+            <Button
+              className="mt-2 px-4 py-2 bg-white-500 text-black rounded hover:bg-white-600"
+              variant="outlined"
+            >
+              Report an issue
+            </Button>
+            <div className="flex flex-col items-center justify-center h-64 md:h-96 bg-gray-200 w-full rounded-lg mb-8 md:mb-12">
+              <span className="text-gray-500 text-xl md:text-2xl">CHAT</span>
+            </div>
+          </div>
+          <button className="w-full bg-blue-600 text-white text-lg md:text-xl p-4 rounded-lg hover:bg-blue-700 transition duration-300">
+            Accept Final Delivery
+          </button>
+        </div>
       </div>
-    );
-  };
-  
-  export default Chat;
-  
+    </div>
+  );
+};
+
+export default Chat;
--- src/Components/Create.tsx
+++ src/Components/Create.tsx
@@ -2,8 +2,11 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Button } from "@mui/material";
 import lighthouse from '@lighthouse-web3/sdk'
 import { useAccount } from "wagmi";
-import dotenv from 'dotenv';
 
+enum Role{
+  Client="Client",
+  Freelancer="Freelancer"
+}
 
 dotenv.config(); // Load environment variables
 
@@ -15,41 +18,45 @@ const Create = () => {
 
   console.log("address", address)
 
-  const handleUpload: any = async() => {
-    const data = `${address}/client/wdID-XXX` 
+  const handleUpload: any = async( role: Role) => {
+    const data = `${address}/${role}/wdID-XXX` 
     const response = await lighthouse.uploadText(data, apiKey, address)
     console.log(response)
   }
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-bold mb-4">ENS Frontend Examples</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="flex flex-col items-center">
-            <Button
-              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              variant="contained"
-              onClick={handleUpload}
-              disabled={!address}
-            >
-              Client
-            </Button>
-          </div>
-          <div className="flex flex-col items-center">
-            <Button
-              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-              variant="contained"
-              disabled={!address}
-            >
-              Freelancer
-            </Button>
-          </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6 py-8">
+    <div className="bg-white rounded-xl shadow-xl p-8 md:p-12 lg:p-16 w-full max-w-4xl space-y-6">
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">ENS Frontend Examples</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="flex flex-col items-center space-y-4">
+          <Button
+            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl"
+            variant="contained"
+            onClick={handleUpload(Role.Client)}
+            disabled={!address}
+            href="/chat"
+          >
+            Client
+          </Button>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <Button
+            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl"
+            variant="contained"
+            disabled={!address}
+            onClick={handleUpload(Role.Freelancer)}
+            href="/chat"
+          >
+            Freelancer
+          </Button>
         </div>
       </div>
-      <div className="flex justify-center">
-        <ConnectButton showBalance={false} />
-      </div>
     </div>
+    <div className="flex justify-center mt-8 md:mt-12">
+      <ConnectButton showBalance={false} />
+    </div>
+  </div>
+  
   );
 };
 
