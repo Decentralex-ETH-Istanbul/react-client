436280d035f7dd13f4187016710ab74ad8413b08
HashRei <hashrei@protonmail.com>
2023-11-19 00:21:18 +0100

    feat: open OpenDisputeGrid in a new tab

--- src/Components/SupportForm.tsx
+++ src/Components/SupportForm.tsx
@@ -82,6 +82,7 @@ const SupportForm: React.FC = () => {
           variant="contained"
           className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors duration-300"
           href='/OpenDisputeGrid'
+          target='_blank'
         >
           Request support
         </Button>
