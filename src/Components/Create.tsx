e02882923c67e1cf3de42ee91856288fc2317a41
stepome <stefanopomelli23@gmail.com>
2023-11-19 05:02:43 +0300

    feat: update the Create design

--- src/Components/Create.tsx
+++ src/Components/Create.tsx
@@ -8,9 +8,7 @@ enum Role{
   Freelancer="Freelancer"
 }
 
-dotenv.config(); // Load environment variables
-
-const apiKey = process.env.REACT_APP_LIGHTHOUSE_API_KEY || ""
+const apiKey =  ""
 
 const Create = () => {
 
@@ -24,13 +22,16 @@ const Create = () => {
     console.log(response)
   }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6 py-8">
-    <div className="bg-white rounded-xl shadow-xl p-8 md:p-12 lg:p-16 w-full max-w-4xl space-y-6">
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">ENS Frontend Examples</h1>
+    
+    <div className="flex flex-col items-center justify-center min-h-screen px-6 py-8 bg-gradient-to-t from-yellow-100 to-indigo-200">
+    <div className="bg-white rounded-xl shadow-l p-8 md:p-12 lg:p-16 w-full max-w-3xl space-y-6">
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-6">Join as a...</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col space-y-4 bg-white">
+          
           <Button
-            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl"
+          
+            className=" login-button px-6 py-3  text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl "
             variant="contained"
             onClick={handleUpload(Role.Client)}
             disabled={!address}
@@ -38,10 +39,12 @@ const Create = () => {
           >
             Client
           </Button>
+          
+          
         </div>
-        <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col space-y-4">
           <Button
-            className="px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl"
+            className=" login-button px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 text-xl md:text-2xl"
             variant="contained"
             disabled={!address}
             onClick={handleUpload(Role.Freelancer)}
